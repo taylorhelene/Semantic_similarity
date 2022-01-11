87a6522b0fa6ef6d45b488f23839ef60459205c0
taylorhelene <taylorhelene09@gmail.com>
2022-01-11 19:51:03 +0300

    first commit

---
+++ semantic similarity.py
@@ -0,0 +1,8 @@
+# Python code to measure similarity between two sentences using cosine similarity. 
+import spacy
+nlp = spacy.load("en")
+# Sentences
+s1 = nlp("The weather is rainy.")
+s2 = nlp("It is going to rain outside.")
+# Calculate the similarity
+print("The similarity is:",s1.similarity(s2))
